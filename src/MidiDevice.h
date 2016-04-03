4b29f673e42e930c4a535056fb3b9d9c71015693
Simon Kallweit <simon@weirdsoft.ch>
2016-04-04 01:30:33 +0200

    added comments

--- src/MidiDevice.h
+++ src/MidiDevice.h
@@ -9,14 +9,20 @@
 #include <algorithm>
 #include <mutex>
 
+//! MIDI device.
 class MidiDevice {
 public:
+    //! Called when the MIDI device is connected.
     virtual void connected() = 0;
+    //! Called when the MIDI device is disconnected.
     virtual void disconnected() = 0;
+    //! Called when a MIDI message is received.
     virtual void handleMessage(const MidiMessage &msg) = 0;
 
+    //! Returns true if the MIDI device is connected.
     bool isConnected() const { return _connected; }
 
+    //! Sends a message.
     void sendMessage(const MidiMessage &msg)
     {
         if (_midiOut.isPortOpen()) {
