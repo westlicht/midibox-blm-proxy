1fd04b743f45c74f66a59ff87821dc009ee418e5
Simon Kallweit <simon@weirdsoft.ch>
2016-04-04 01:30:39 +0200

    added comments

--- src/Midi.h
+++ src/Midi.h
@@ -4,14 +4,22 @@
 
 #include <string>
 
+//! MIDI system.
 class Midi {
 public:
+    //! Updates the MIDI system.
+    //! Checks if registered MIDI devices are connected/disconnected.
+    //! Needs to be called periodically.
     static void update();
 
+    //! Registers a MIDI device.
     static void addDevice(MidiDevice *device, const std::string &name);
+    //! Unregisters a MIDI device.
     static void removeDevice(MidiDevice *device);
 
+    //! Returns a list of all detected MIDI input ports.
     static const std::vector<std::string> &inputPorts() { return _inputPorts; }
+    //! Returns a list of all detected MIDI output ports.
     static const std::vector<std::string> &outputPorts() { return _outputPorts; }
 
 private:
