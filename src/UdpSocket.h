bf81d23ed8a95bad6ac0b9fe237315b1aabb8ce5
Simon Kallweit <simon@weirdsoft.ch>
2016-04-04 01:32:17 +0200

    added comments

--- src/UdpSocket.h
+++ src/UdpSocket.h
@@ -2,14 +2,27 @@
 
 #include <string>
 
+//! UDP socket.
 class UdpSocket {
 public:
+    //! Constructor.
     UdpSocket();
+    //! Destructor.
     ~UdpSocket();
 
+    //! Connects the socket to the given host.
+    //! Opens both a port for reading and writing.
     bool connect(const std::string &host, int portRead, int portWrite);
+
+    //! Disconnects the socket.
     void disconnect();
+
+    //! Writes a datagram.
+    //! Returns the number of bytes sent.
     int write(const unsigned char *data, int len);
+
+    //! Reads a datagram.
+    //! Returns the number of bytes received, zero if no datagram is available.
     int read(unsigned char *data, int len);
 
 protected:
